5f3c11cfdac06e774519a030da41902d4f7f6c7f
Harshit <harshitupadhyay872@gmail.com>
2023-05-29 23:55:26 +0530

    Updates

---
+++ precache-manifest.3a4c05feb601d24766b89d288b52ca60.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d49616c4988321cac7d",
+    "url": "/addy-mobile24/static/css/main.20965c0c.chunk.css"
+  },
+  {
+    "revision": "4d49616c4988321cac7d",
+    "url": "/addy-mobile24/static/js/main.4d49616c.chunk.js"
+  },
+  {
+    "revision": "5bff56686b744a6e6287",
+    "url": "/addy-mobile24/static/js/1.5bff5668.chunk.js"
+  },
+  {
+    "revision": "718bb3fb57ce9d31f583",
+    "url": "/addy-mobile24/static/css/2.6d043b7f.chunk.css"
+  },
+  {
+    "revision": "718bb3fb57ce9d31f583",
+    "url": "/addy-mobile24/static/js/2.718bb3fb.chunk.js"
+  },
+  {
+    "revision": "d547d3f1d05773cde888",
+    "url": "/addy-mobile24/static/js/runtime~main.d547d3f1.js"
+  },
+  {
+    "revision": "f35f5aeb25f479b79f9f07221bdbc8f7",
+    "url": "/addy-mobile24/static/media/home.f35f5aeb.png"
+  },
+  {
+    "revision": "355b4f01a0178c0136db1404dbe97c36",
+    "url": "/addy-mobile24/static/media/cantact.355b4f01.png"
+  },
+  {
+    "revision": "195a55ab09f4e4f4cdf76c30996704ca",
+    "url": "/addy-mobile24/static/media/logo.195a55ab.png"
+  },
+  {
+    "revision": "aed14c0ac0d8bb99f978d2e3378a778c",
+    "url": "/addy-mobile24/static/media/white-logo.aed14c0a.png"
+  },
+  {
+    "revision": "dd0c82b575d3ba767ae2fca4b082d577",
+    "url": "/addy-mobile24/static/media/white-user.dd0c82b5.png"
+  },
+  {
+    "revision": "ae0bfa82f983cf1832c7038bac0fb9c5",
+    "url": "/addy-mobile24/static/media/auto-ewallet.ae0bfa82.jpg"
+  },
+  {
+    "revision": "a2465140fa88edf31673a01e47788710",
+    "url": "/addy-mobile24/static/media/online.a2465140.jpg"
+  },
+  {
+    "revision": "4b0662c4c4bd847e9aca903917f387fa",
+    "url": "/addy-mobile24/static/media/ATM.4b0662c4.jpg"
+  },
+  {
+    "revision": "50cb4574b6ee7e4d8683c779f2a16d42",
+    "url": "/addy-mobile24/index.html"
+  }
+];
